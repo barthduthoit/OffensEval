aff92165d199c07c5a01f51636b95180146adbe3
Barthélemy DUTHOIT <barthelemyduthoit@dyn3143-33.wlan.ic.ac.uk>
2019-02-14 09:30:14 +0000

    remove tokenization/stemming in preprocessing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -8,8 +8,8 @@ from nltk.stem.porter import *
 stemmer = PorterStemmer()
 other_stopwords = ['also', 'another', 'came', 'come', 'could ', 'even', 'furthermore', 'get',
                   'got', 'hi', 'however', 'indeed', 'like', 'made', 'many', 'might', 'moreover',
-                  'much', 'must', 'my never', 'now of', 'said', 'see', 'since', 'still', 'take',
-                  'therefore', 'thus', 'way', 'well', 'would', 'null', 'url']
+                  'much', 'must', 'my', 'now of', 'said', 'see', 'since', 'still', 'take',
+                  'therefore', 'thus', 'way', 'would', 'null', 'url']
 stopwords_all = other_stopwords + stopwords.words('english')
 stopwords_regex = r"\b({})\b".format('|'.join(map(re.escape, stopwords_all)))
 
@@ -25,9 +25,9 @@ def clean(df):
     df["clean_tweets"] = df["clean_tweets"].str.replace(r"\s\s+"," ")
     
     # Tokenization
-    df["tokens"] = df["clean_tweets"].str.split()
-    df["tokens"] = df["tokens"].apply(lambda x: [stemmer.stem(i) for i in x])
-    df["clean_tweets"] = df["tokens"].apply(lambda x: " ".join(x))
+    #df["tokens"] = df["clean_tweets"].str.split()
+    #df["tokens"] = df["tokens"].apply(lambda x: [stemmer.stem(i) for i in x])
+    #df["clean_tweets"] = df["tokens"].apply(lambda x: " ".join(x))
     
     # droping empty rows
     df = df[df["clean_tweets"].str.len()>0]
