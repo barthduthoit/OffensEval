9158f59505142ee15eac199ec44d3ad3c7f85678
Barthélemy DUTHOIT <barthelemyduthoit@MacBook-de-Barthelemy-DUTHOIT.local>
2019-02-22 15:30:48 +0000

    fix typo

--- utils/test_model.py
+++ utils/test_model.py
@@ -21,13 +21,13 @@ def single_fold_keras(model, X_train, X_test, y_train, y_test, tokenizer):
     tokenizer.fit_on_texts(X_train)
     list_tokenized_train = tokenizer.texts_to_sequences(X_train)
     list_tokenized_test = tokenizer.texts_to_sequences(X_test)
-    X_t = sequence.pad_sequences(list_tokenized_train, maxlen=100)
-    X_te = sequence.pad_sequences(list_tokenized_test, maxlen=100)
+    X_tr = sequence.pad_sequences(list_tokenized_train, maxlen=200)
+    X_te = sequence.pad_sequences(list_tokenized_test, maxlen=200)
     
     clf = keras.models.clone_model(model)
     clf.set_weights(model.get_weights())
     clf.compile(loss=f1_loss, optimizer='adam', metrics=['accuracy'])
-    clf.fit(X_t, y_train_cat, epochs=5,  batch_size=64,  validation_data=(X_te, y_test_cat))
+    clf.fit(X_tr, y_train_cat, epochs=5,  batch_size=64,  validation_data=(X_te, y_test_cat))
     
     y_pred_proba = clf.predict(X_te)
     return np.argmax(y_pred_proba, axis=1)
@@ -47,7 +47,7 @@ def test_single_model(model, X, y, vec, n_splits=3, random_state=1):
         y_train, y_test = y[train_index], y[test_index]
         if type(model)==keras.engine.sequential.Sequential:
             y_pred = single_fold_keras(model, X_train, X_test, y_train, y_test, vec)
-        elif str(type(model)).contains('sklearn'):
+        elif 'sklearn' in str(type(model)):
             y_pred = single_fold_sklearn(model, X_train, X_test, y_train, y_test, vec)
         else:
             print("Model not supported")
